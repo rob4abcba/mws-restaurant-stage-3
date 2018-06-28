d1d0272c67931bd0be65ffc0d0fc4c38143079f2
Rob ABCBA <rob_abcba@yahoo.com>
2018-06-28 03:39:03 -0700

    Add flexbox. Add media queries.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,8 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        // RL mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoicm9iNGFiY2JhIiwiYSI6ImNqaXFqZzN5cTE5dm0zcXQ4N3FsZ2NtbGMifQ.ZSP-BO8phKgF0poYo62xaA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
